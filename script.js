dea8e5664fe361605d76adba72bb2285e2a2ab6f
jdegand <70610011+jdegand@users.noreply.github.com>
2021-03-17 11:48:15 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let email = document.getElementById("email");
+let exclamationPoint = document.getElementById('exclamation-point');
+let errorMessage = document.getElementById("error-message");
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+    if (email.validity.typeMismatch) {
+        e.preventDefault();
+        errorMessage.classList.remove('hidden');
+        exclamationPoint.style.opacity = 1;
+        errorMessage.classList.add('display-block');
+    } else {
+        errorMessage.classList.remove('display-block');
+        exclamationPoint.style.opacity = 0;
+        errorMessage.classList.add('hidden');
+    }
+})
